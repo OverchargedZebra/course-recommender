4a52ca037f536bda9cb9dd13688d1e25bbc76800
OverchargedZebra <waleedbinshabbir@gmail.com>
2025-08-28 23:53:16 +0500

    chore: - removed build tools, they were not needed - made improvements to the nginx config which was hindering performance

--- frontend/build_tools/patch_web.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-patch build/web/flutter.js < build_tools/web/flutter.js.patch
-patch build/web/flutter_service_worker.js < build_tools/web/flutter_service_worker.js.patch
